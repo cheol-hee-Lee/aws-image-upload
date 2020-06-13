3d5c534a466ae8a25a3f23878caa446817fcf306
cheol-hee-Lee <201802141@o.cnu.ac.kr>
2020-06-13 16:07:05 +0900

    Add upload code to UserProfileService.java Update code to extract method

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,7 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios"
+import {useDropzone} from 'react-dropzone'
 
 const UserProfiles = () => {
   const [userProfiles, setUserProfiles] = useState([]);
@@ -22,12 +23,56 @@ const UserProfiles = () => {
 
     return (
       <div key={index}>
+        {/* todo : profile image */}
+        <br/>
+        <br/>
         <h1>{userProfile.username}</h1>
         <p>{userProfile.userProfileId}</p>
+        <br/>
+        <Dropzone {...userProfile}/>
       </div>
     )
   });
 }
+
+function Dropzone({userProfileId}) {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+    const file = acceptedFiles[0];
+    console.log(file);
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    axios.post(
+      `http://localhost:8080/api/v1/user-profile/${userProfileId}/image/upload`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      }
+      ).then(() => {
+        console.log("file uploaded successfully");
+      }).catch(err => {
+        console.log(err);
+      });
+
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the image here ...</p> :
+          <p>Drag 'n' drop profile image, or click to select profile image</p>
+      }
+    </div>
+  )
+}
+
 
 function App() {
   return (
